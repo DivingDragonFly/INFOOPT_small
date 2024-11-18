e21a8ce9cdd573bb8accbefd1890ba5bf52014f8
Aki <a.u.ishizuka@students.nl>
2024-11-18 17:57:43 +0100

    Test console app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter name:");
+string a = Console.ReadLine();
+Console.WriteLine($"Your name is {a}");
